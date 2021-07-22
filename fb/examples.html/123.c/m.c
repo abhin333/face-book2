f9c9523d3c3d2379354c6eed8513ced1a881f1b8
abhin <abhinpradeepan123@gmail.com>
2021-07-22 04:31:57 -1100

    face book
    just like a face book page

---
+++ fb/examples.html/123.c/m.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello, World!\n");
+    getch(); //Use to get one character input from user, and it will not be printed on screen.
+    return 0;
+}
+html
